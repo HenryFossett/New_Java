34eb5f881b2774bec3c0b5bf269d081e4568393d
Henry Fossett <118143819+HenryFossett@users.noreply.github.com>
2023-09-12 14:33:44 -0400

    Add files via upload
    initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 22;
+        String greeting = "Hello Joe, watta you know?";
+        System.out.println(greeting);
+        System.out.println(greeting + " got beer?!");
+    }
+}
